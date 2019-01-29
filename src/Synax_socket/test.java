5b4dc5cb869f4f22f95c61560e87abc0f2632823
wecallitabill <m18752095913@163.com>
2019-01-29 23:15:04 +0800

    Java Syntax Practice

---
+++ src/Synax_socket/test.java
@@ -0,0 +1,14 @@
+package Synax_socket;
+
+import java.util.Scanner;
+
+public class test {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(scanner.nextLine());
+		
+
+		
+	}
+}
