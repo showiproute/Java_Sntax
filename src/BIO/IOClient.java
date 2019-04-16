b38d886ee88db6e8340d130a2e65ffeb0abc9d93
wecallitabill <m18752095913@163.com>
2019-04-16 14:43:10 +0800

    Java_Synax Review

---
+++ src/BIO/IOClient.java
@@ -0,0 +1,65 @@
+package BIO;
+
+import java.net.Socket;
+import java.util.Date;
+
+import BIO.IOClient.ThreadClient;
+import BIO.IOServer.ServerHandler;
+
+public class IOClient {
+
+	public static final int PORT=3333;
+	public static final String HOSTNAME="127.0.0.1";
+	
+	public static class ThreadClient extends Thread{
+		
+		public static String ThreadClientName;
+		public static int counts=0;
+		
+		public ThreadClient(String name) {
+			// TODO Auto-generated constructor stub
+			this.ThreadClientName=name;
+		}
+		
+		
+		@SuppressWarnings("resource")
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				Socket socket = new Socket(HOSTNAME,PORT);
+				while(true) {
+					try {
+						socket.getOutputStream().write((new Date()+":from client!"+this.ThreadClientName).getBytes());
+						Thread.sleep(5000);
+						counts++;
+						if(counts==5) {
+							ThreadClient threadClient2 = new ThreadClient("线程2");
+							new Thread(threadClient2).start();
+						}
+					}catch (Exception e) {
+						// TODO: handle exception
+						e.printStackTrace();
+					}
+				}
+				
+			}catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		ThreadClient threadClient = new ThreadClient("线程1");
+		new Thread(threadClient).start();;
+		
+		Thread.sleep(5000);
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ src/BIO/IOServer.java
@@ -0,0 +1,79 @@
+package BIO;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import BIO.IOServer.ThreadServer;
+
+
+public class IOServer {
+	
+	public static final int PORT=3333;
+	
+	public static ServerSocket serverSocket=null;
+	
+	public static class ThreadServer extends Thread{
+		
+		public ThreadServer(String name) {
+			// TODO Auto-generated constructor stub
+			super(name);
+		}
+		
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				serverSocket=new ServerSocket(PORT);
+				ServerHandler serverHandler = new ServerHandler();
+				new Thread(serverHandler,"threadServer").start();
+					
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public static class ServerHandler extends Thread{
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while(true) {
+				try {
+					Socket socket = serverSocket.accept();
+					
+					try {
+						int len;
+						byte[] data=new byte[1024];
+						InputStream inputStream = socket.getInputStream();
+						
+						while((len=inputStream.read(data))!=-1) {
+							System.out.println(new String(data,0,len));
+						}
+					}catch (Exception e) {
+						// TODO: handle exception
+						e.printStackTrace();
+					}
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		ThreadServer threadServer = new ThreadServer("服务端线程1");
+		new Thread(threadServer).start();
+		
+	}
+	
+}
